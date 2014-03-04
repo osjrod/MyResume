45fec05f1ab8512b2bf1c0695858e69035fc61fa
osjrod <osjrod@hotmail.com>
2014-03-04 13:20:48 -0600

    Listo para entrega r1

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+
+    $('#salir').click(function () {
+        $('#myLogin').modal('hide');
+    });
+
+    $(document).mousemove(function (e) {
+        TweenLite.to($('#log'),
+           .5,
+           {
+               css:
+                 {
+                     'background-position': parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / 12) + "px, " + parseInt(event.pageX / 15) + "px " + parseInt(event.pageY / 15) + "px, " + parseInt(event.pageX / 30) + "px " + parseInt(event.pageY / 30) + "px"
+                 }
+           });
+    });
+
+
+    $("#tel").keydown(function (e) {
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            return;
+        }
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+});
